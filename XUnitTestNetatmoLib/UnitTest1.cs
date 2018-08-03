e6add2f74b4bf67d7f4aca1961447dc6c5810a06
Kaspars Mickevics <kaspars@fx.lv>
2018-08-03 14:17:51 +0300

    Renamed netatmo to NetatmoCLI and added XUnit test
    No tests written yet, but prepared the solution and added
    a project for that.

---
+++ XUnitTestNetatmoLib/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace XUnitTestNetatmoLib
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var netAuth = new netatmo.NetatmoAuth();
+
+        }
+        }
+}
