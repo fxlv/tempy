cc076245103ed0beee95ab30cc53f8be04a7870d
Kaspars Mickēvičs <kaspars@fx.lv>
2020-10-25 14:36:22 +0200

    Docker (#2)
    * adding pod definitions

--- TempyWorker/start.sh
+++ TempyWorker/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -e /config/appsettings.json ]; then
+    echo -n "Copying configuration file: "
+    cp -v /config/appsettings.json .
+fi
 echo "Tempy API target: $TEMPY_API_TARGET"
 dotnet run
-
