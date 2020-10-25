a26a177ece0c00ae1de6ac2e46f60ee4aff1cbf3
Kaspars Mickēvičs <kaspars@fx.lv>
2020-10-25 16:38:45 +0200

    Kube configuration (#3)
    * adding pod definitions
    * adding deployments, service and build scripts

--- TempyWorker/start.sh
+++ TempyWorker/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ -e /config/appsettings.json ]; then
+    echo "Running in a Kubernetes environment"
     echo -n "Copying configuration file: "
     cp -v /config/appsettings.json .
+    export TEMPY_API_TARGET=tempy-api:5000
 fi
 echo "Tempy API target: $TEMPY_API_TARGET"
 dotnet run
